e7e6303c01b9ec165b02a3a23930d207ac6ccaa1
Keith C. Ivey <keith@iveys.org>
2019-05-01 10:11:54 -0400

    move web files to public subdir and add build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cp -au node_modules/jquery/dist/jquery.min.js public/js/
+cp -au node_modules/leaflet/dist/leaflet.js public/js/
+cp -au node_modules/leaflet/dist/leaflet.css public/css/
