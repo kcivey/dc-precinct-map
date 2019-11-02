cafc789b6de6a7a58f8c2fa158529509059cf51f
Keith C. Ivey <keith@iveys.org>
2019-11-02 19:56:21 -0400

    tweak public/index.js

--- public/index.js
+++ public/index.js
@@ -5,11 +5,14 @@ jQuery(function ($) {
         })
         .then(function (geoJson) {
             const tileLayer = L.tileLayer('https://{s}.tiles.mapbox.com/v3/kcivey.i8d7ca3k/{z}/{x}/{y}.png', {
-                attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>',
+                attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> ' +
+                    '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> ' +
+                    '<strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">' +
+                    'Improve this map</a></strong>',
                 opacity: 0.5,
             });
-            const data = i => i % 2 ? 1 : 0;
             const style = function (feature, value) {
+                value = Math.random() < 0.5 ? 0 : 1;
                 return {
                     fillColor: value ? 'black' : 'white',
                     color: value ? 'white' : 'black',
@@ -17,6 +20,14 @@ jQuery(function ($) {
                     fillOpacity: 0.6,
                 };
             };
-            new DcMap(geoJson, {tileLayer, style, data, usePopups: false}).display();
+            const map = new DcMap(geoJson, {tileLayer, style, data, usePopups: false}).display();
+            // setInterval(setData, 1000);
+            function setData() {
+                const data = [];
+                for (let i = 0; i < 144; i++) {
+                    data.push(Math.random() < 0.5 ? 0 : 1);
+                }
+                map.setData(data);
+            }
         });
 });
